fff431d839fd94a19ac794d0bd7109e541e10d35
VictorOkereke-dev <vicdocsbackup080@gmail.com>
2025-08-17 18:09:40 +0100

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,1 +1,20 @@
+const app = require("./app");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 
+dotenv.config();
+
+const PORT = process.env.PORT || 3000;
+
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}).then(() => {
+  console.log("MongoDB connected");
+  app.listen(PORT, () => {
+    console.log(`InventoryHub running on http://localhost:${PORT}`);
+  });
+}).catch(err => {
+  console.error("MongoDB connection error:", err);
+});
+    
