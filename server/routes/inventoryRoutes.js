23a20a3a2ebe08fa5dba5d85c8ed59a548d36ea3
VictorOkereke-dev <vicdocsbackup080@gmail.com>
2025-08-17 18:20:14 +0100

    Update inventoryRoutes.js

--- server/routes/inventoryRoutes.js
+++ server/routes/inventoryRoutes.js
@@ -1,1 +1,11 @@
+const express = require("express");
+const router = express.Router();
+const { addItem, getItems, updateItem } = require("../controllers/inventoryController");
+const { authenticate, authorize } = require("../middleware/authMiddleware");
 
+router.get("/", authenticate, getItems);
+router.post("/", authenticate, authorize("admin"), addItem);
+router.put("/:id", authenticate, authorize("admin"), updateItem);
+
+module.exports = router;
+            
