66154cc5a09314b5b6fa4b0b0bb440e96649937a
VictorOkereke-dev <vicdocsbackup080@gmail.com>
2025-08-17 18:17:50 +0100

    Update inventoryController.js

--- server/controllers/inventoryController.js
+++ server/controllers/inventoryController.js
@@ -1,1 +1,21 @@
+const Item = require("../models/Item");
 
+exports.addItem = async (req, res) => {
+  const { name, quantity, price } = req.body;
+  const item = new Item({ name, quantity, price });
+  await item.save();
+  res.json({ message: "Item added", item });
+};
+
+exports.getItems = async (req, res) => {
+  const items = await Item.find().sort({ updatedAt: -1 });
+  res.json(items);
+};
+
+exports.updateItem = async (req, res) => {
+  const { id } = req.params;
+  const { quantity, price } = req.body;
+  const item = await Item.findByIdAndUpdate(id, { quantity, price, updatedAt: Date.now() }, { new: true });
+  res.json({ message: "Item updated", item });
+};
+                         
