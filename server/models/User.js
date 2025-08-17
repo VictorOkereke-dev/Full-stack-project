3a5b9b617d46fa3e613d0e0bb5548994826acaa9
VictorOkereke-dev <vicdocsbackup080@gmail.com>
2025-08-17 18:12:55 +0100

    Update User.js

--- server/models/User.js
+++ server/models/User.js
@@ -1,1 +1,10 @@
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema({
+  username: { type: String, unique: true },
+  password: String,
+  role: { type: String, enum: ["admin", "user"], default: "user" }
+});
+
+module.exports = mongoose.model("User", UserSchema);
 
