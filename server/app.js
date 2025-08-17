9595d07667145001b8d8e9cede297bb22a055e66
VictorOkereke-dev <vicdocsbackup080@gmail.com>
2025-08-17 18:11:37 +0100

    Update app.js

--- server/app.js
+++ server/app.js
@@ -1,1 +1,15 @@
+const express = require("express");
+const path = require("path");
+const authRoutes = require("./routes/authRoutes");
+const inventoryRoutes = require("./routes/inventoryRoutes");
 
+const app = express();
+
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "../client")));
+
+app.use("/api/auth", authRoutes);
+app.use("/api/inventory", inventoryRoutes);
+
+module.exports = app;
+                       
